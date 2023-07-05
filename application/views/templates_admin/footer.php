58db8239e1056c393f44de74d1aa68899dcf97f8
171202 <138703886+171202@users.noreply.github.com>
2023-07-05 23:31:02 +0700

    Add files via upload

---
+++ application/views/templates_admin/footer.php
@@ -0,0 +1,30 @@
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?php echo base_url() ?>assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?php echo base_url() ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?php echo base_url() ?>assets/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="<?php echo base_url() ?>assets/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="<?php echo base_url() ?>assets/js/demo/chart-area-demo.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/demo/chart-pie-demo.js"></script>
+
+    <footer class="footer bg-brown text-white">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center">
+                    &copy; 2023 Web by Kelompok 6
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    </body>
+
+    </html>
